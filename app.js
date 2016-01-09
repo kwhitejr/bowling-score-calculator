eebb856658763d9c880e9f8ad77dac49e0ba1a75
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-09 11:29:13 -1000

    once more, with feeling

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
-module.exports = function bowlingScoreCalc() {
-
-  this.currentScore = 0;
-
-  this.frame = this.frameTracker.length;
-
-  this.frameTracker = [];
-
-  this.bowlingScore = function(pinList) {
-
-    frameTracker.reduce(function(firstFrame, nextFrame) {
-      if (firstFrame[0].length === 10) {
-        // strike
-      } else if (firstFrame[0].length + firstFrame[1].length === 10) {
-        // spare
-      } else {
-        return (firstFrame[0].length + firstFrame[1].length) + (nextFrame[0].length + nextFrame[1].length);
-      }
-    });
-
-  };
-
-  this.spare = function(currentScore, nextBowl) {
-
-  };
-
-  this.strike = function(currentScore, nextTwoBowls) {
-
-  };
+module.exports = {
+  getFrames: getFrames,
+  getScore: getScore
 };
+
+function getFrames (rolls) {
+  var allFrames = [];
+  for (var i=0; i < rolls.length; i++) {
+    var newFrame =[];
+    newFrame.push(rolls[i]);
+    if (rolls[i] < 10) {
+      newFrame.push(rolls[i+1]);
+      i++;
+    }
+    allFrames.push(newFrame);
+  }
+  return allFrames;
+}
+
+function getScore (frames) {
+  var score = 0;
+  for (var i = 0; i < frames.length; i++) {
+    if (i[0] === 10) {
+      //strike
+    } else if (i[0] + i[1] === 10) {
+      //spare
+    } else {
+      //reg
+    }
+  }
+}
+
+var worstGame = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; // score 0
+var oneGame = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]; // score 20
+var fiveGame = [5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]; // score ?
+var perfectGame = [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]; // score 300
+
+
+console.log(getFrames(worstGame));
+console.log(getFrames(oneGame));
