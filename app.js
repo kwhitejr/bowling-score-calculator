97672d28fe56b09193b51eafc10e67367e7b98d3
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-09 10:51:23 -1000

    switched to complete frame tacking with nested arrays; using reduce to calc score

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 module.exports = {
-  bowlingScore: function(downPins) {
 
+  currentScore: 0,
+
+  frame: frameTracker.length,
+
+  frameTracker: [],
+
+  bowlingScore: function(firstDownPins, secondDownPins) {
+    this.frameTracker.push([firstDownPins, secondDownPins]);
+
+    frameTracker.reduce(function(firstFrame, nextFrame) {
+      if (firstFrame[0].length === 10) {
+        // strike
+      } else if (firstFrame[0].length + firstFrame[1].length === 10) {
+        // spare
+      } else {
+        return (firstFrame[0].length + firstFrame[1].length) + (nextFrame[0].length + nextFrame[1].length);
+      }
+    });
+
+
+    console.log('current score: ' + this.currentScore);
+    console.log('current frame: ' + this.frame);
+    console.log('frameTracker: ' + this.frameTracker);
   },
 
   spare: function(currentScore, nextBowl) {
 
+
+    nextBowl = this.bowlingScore;
+    var previousFrameScore = currentScore + nextBowl.length + 10;
   },
 
-  strike: function() {
-
+  strike: function(currentScore, nextTwoBowls) {
+    var previousFrameScore = currentScore + this.bowlingScore();
   }
 };
--- test/app-spec.js
+++ test/app-spec.js
@@ -5,6 +5,85 @@ var bowlingScore = module.bowlingScore;
 var spare = module.spare;
 var strike = module.strike;
 var frame = module.frame;
+var currentScore = module.currentScore;
+var frameTracker = module.frameTracker;
+
+
+describe('currentScore', function () {
+
+  it('should be a number', function () {
+    expect(frame).to.be.a('number');
+  });
+
+  it('should keep add non spare/strike scores together', function () {
+    var pinsDown1 = [1, 2, 3];
+    var pinsDown2 = [4, 5, 6];
+    var pinsDown3 = [1, 2, 3];
+    var pinsDown4 = [6, 7, 8];
+    var pinsDown5 = [1, 2, 3];
+    var pinsDown6 = [4, 5, 6];
+    bowlingScore(pinsDown1, pinsDown2);
+    bowlingScore(pinsDown3, pinsDown4);
+    bowlingScore(pinsDown5, pinsDown6);
+    expect(currentScore).to.equal(18);
+  });
+
+  it('should add a bonus for spare', function () {
+    var pinsDown1 = [1, 2, 3];
+    var pinsDown2 = [4, 5, 6];
+    var pinsDown3 = [1, 2, 3, 4, 5];
+    var pinsDown4 = [6, 7, 8, 9, 10];
+    var pinsDown5 = [1];
+    var pinsDown6 = [2];
+    bowlingScore(pinsDown1, pinsDown2);
+    bowlingScore(pinsDown3, pinsDown4);
+    bowlingScore(pinsDown5, pinsDown6);
+    expect(currentScore).to.equal(19);
+  });
+
+  it('should add a bonus for strike', function () {
+    var pinsDown1 = [1, 2, 3];
+    var pinsDown2 = [4, 5, 6];
+    var pinsDown3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var pinsDown4 = [1];
+    var pinsDown5 = [2];
+    bowlingScore(pinsDown1, pinsDown2);
+    bowlingScore(pinsDown3, pinsDown4);
+    bowlingScore(pinsDown5, pinsDown6);
+    expect(currentScore).to.equal(20);
+  });
+
+});
+
+describe('frame', function () {
+
+  it('should be a number', function () {
+    expect(frame).to.be.a('number');
+  });
+
+  it('should keep track of simple frame status', function () {
+    var pinsDown1 = [1, 2, 3];
+    var pinsDown2 = [4, 5, 6];
+    var pinsDown3 = [1, 2, 3, 4, 5];
+    var pinsDown4 = [6, 7, 8, 9, 10];
+    bowlingScore(pinsDown1, pinsDown2);
+    bowlingScore(pinsDown3, pinsDown4);
+    expect(frame).to.equal(3);
+  });
+
+  it('should keep track of frame status with strike', function () {
+    var pinsDown1 = [1, 2, 3];
+    var pinsDown2 = [4, 5, 6];
+    var pinsDown3 = [1, 2, 3, 4, 5];
+    var pinsDown4 = [6, 7, 8, 9, 10];
+    var pinsDown5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    bowlingScore(pinsDown1, pinsDown2);
+    bowlingScore(pinsDown3, pinsDown4);
+    bowlingScore(pinsDown5);
+    expect(frame).to.equal(4);
+  });
+
+});
 
 describe('bowlingScore', function () {
 
@@ -15,27 +94,6 @@ describe('bowlingScore', function () {
 
 });
 
-describe('frame', function () {
-
-  it('should be a number', function () {
-    expect(frame).to.be.a('number');
-  });
-
-  it('should keep track of frame status', function () {
-    var pinsDown1 = [1, 2, 3];
-    var pinsDown2 = [4, 5, 6];
-    var pinsDown3 = [1, 2, 3, 4, 5];
-    var pinsDown4 = [6, 7, 8, 9, 10];
-    var pinsDown5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    bowlingScore(pinsDown1);
-    bowlingScore(pinsDown2);
-    bowlingScore(pinsDown3);
-    bowlingScore(pinsDown4);
-    bowlingScore(pinsDown5);
-    expect(frame).to.equal(3);
-  });
-
-});
 
 describe('spare', function () {
 
