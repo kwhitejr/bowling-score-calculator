ce51c3555c892190d7cf84d065e6dfc324539e4d
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-09 13:44:12 -1000

    working on dealing with last frame

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ function getFrames (rolls) {
     newFrame.push(rolls[i]);
     if (rolls[i] < 10) {
       newFrame.push(rolls[i+1]);
+      if (allFrames.length === 9) {
+        newFrame.push(rolls[i+2]);
+        i++;
+      }
       i++;
+    }
+    if (i === 9 && rolls[i] === 10) {
+      newFrame.push(rolls[i+1], rolls[i+2]);
+      i+=2;
     }
     allFrames.push(newFrame);
   }
@@ -19,22 +27,34 @@ function getFrames (rolls) {
 
 function getScore (frames) {
   var score = 0;
-  for (var i = 0; i < frames.length; i++) {
-    if (i[0] === 10) {
-      //strike
-    } else if (i[0] + i[1] === 10) {
-      //spare
+  for (var i=0; i<frames.length; i++) {
+    if (frames[i] === 10) {
+      // strike
+    } else if (frames[i] + frames[i+1] === 10) {
+      // spare
+      score += (10 + frames[i+2]);
+      i++;
+      console.log(score);
     } else {
-      //reg
+      score += frames[i] + frames[i+1];
+      i++;
     }
   }
+  return score;
+}
+
+function lastFrameHandler (rolls) {
+  var frames = getFrames(rolls);
+  var lastFrame = frames[9];
+  return lastFrame;
 }
 
 var worstGame = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; // score 0
 var oneGame = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]; // score 20
-var fiveGame = [5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]; // score ?
-var perfectGame = [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]; // score 300
+var fiveGame = [5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]; // score 150
+var perfectGame = [10,10,10,10,10,10,10,10,10,10,10,10]; // score 300
 
 
-console.log(getFrames(worstGame));
-console.log(getFrames(oneGame));
+// console.log(getScore(worstGame));
+// console.log(getScore(oneGame));
+console.log(getScore(fiveGame));
--- test/app-spec.js
+++ test/app-spec.js
@@ -1,4 +1,55 @@
 var chai = require('chai');
 var expect = chai.expect;
-var module = require('../app');
+var calculator = require('../app');
 
+describe('getFrames', function () {
+  it('should be a function', function () {
+
+  });
+
+  it('should group together rolls by frame', function () {
+    var worstGame = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; // score 0
+    expect(calculator.getFrames(worstGame)).to.have.length(10);
+
+    var oneGame = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]; // score 20
+    expect(calculator.getFrames(oneGame)).to.have.length(10);
+
+  });
+
+  it('should permit one extra roll on final spare', function () {
+    var fiveGame = [5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5, 5]; // score 150
+    expect(calculator.getFrames(fiveGame)).to.have.length(10);
+  });
+
+  it('should permit two extra rolls on final strike', function () {
+    var perfectGame = [10,10,10,10,10,10,10,10,10,10,10,10]; // score 300
+    expect(calculator.getFrames(perfectGame)).to.have.length(10);
+  });
+
+});
+
+describe('getScore', function () {
+  it('should be a function', function () {
+
+  });
+
+  it('should add up all the frames', function () {
+    var worstGame = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; // score 0
+    expect(calculator.getScore(worstGame)).to.equal(0);
+
+    var oneGame = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]; // score 20
+    expect(calculator.getScore(oneGame)).to.equal(20);
+
+  });
+
+  it('should give a bonus for spare', function () {
+    var fiveGame = [5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]; // score ?
+    expect(calculator.getScore(fiveGame)).to.equal(150);
+  });
+
+  it('should give a bonus for strike', function () {
+    var perfectGame = [10,10,10,10,10,10,10,10,10,10]; // score 300
+    expect(calculator.getScore(perfectGame)).to.equal(300);
+  });
+
+});
